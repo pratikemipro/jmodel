11558f48579e691b758948359c02644ecc75a3ac
sharpblue100 <sharpblue100@cd21a03a-4e8f-11de-84bb-6bba061bd1d3>
2009-07-17 12:11:27 +0000

    Fixed embarrassingly amateurish bug in implementation of function ordering.

--- jmodel.js
+++ jmodel.js
@@ -1383,14 +1383,16 @@ var jModel = function () {
 	
 	
 	var FunctionOrdering = external.func = function(fn) {
-		if ( fn(a) < fn(b) ) {
-			return -1;
-		}
-		else if ( fn(a) > fn(b) ) {
-			return 1;
-		}
-		return 0;
-	}
+		return function (a,b) {
+			if ( fn(a) < fn(b) ) {
+				return -1;
+			}
+			else if ( fn(a) > fn(b) ) {
+				return 1;
+			}
+			return 0;
+		};
+	};
 	
 	
 	var FieldOrdering = external.field = function (fieldName,getter) {
