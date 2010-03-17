45c484626cca4a2be7e72dc19bd456f5aba2dfd2
sharpblue100 <sharpblue100@cd21a03a-4e8f-11de-84bb-6bba061bd1d3>
2010-03-17 20:56:30 +0000

    Corrected header comment of schema plugin.

--- trunk/plugins/jmodel.schema.js
+++ trunk/plugins/jmodel.schema.js
@@ -1,11 +1,9 @@
 /*
- *	jModel OData Plugin v0.1.0
+ *	jModel Schema Plugin v0.1.0
  *	http://code.google.com/p/jmodel/
  *
  *	Copyright (c) 2010 Richard Baker
  *	Dual licensed under the MIT and GPL licenses
- *
- *  Requires jQuery
  *
  */
  
