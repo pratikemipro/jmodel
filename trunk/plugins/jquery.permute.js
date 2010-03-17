85c27b4a1c5237754aeab8b83ecbaf0f31923596
sharpblue100 <sharpblue100@cd21a03a-4e8f-11de-84bb-6bba061bd1d3>
2010-03-17 21:06:46 +0000

    Separated jQuery plugins into separate files.

---
+++ trunk/plugins/jquery.permute.js
@@ -0,0 +1,29 @@
+/*
+ *	jQuery Permutation Plugin v0.1.0
+ *	http://code.google.com/p/jmodel/
+ *
+ *	Copyright (c) 2010 Richard Baker
+ *	Dual licensed under the MIT and GPL licenses
+ *
+ */
+
+jQuery.fn.permute = function (permutation) {
+
+	if ( this.length != permutation.length ) {
+		return false;
+	}
+
+	var copies = [], placeholders = [], i;
+	for (i=0; i<this.length; i++) {
+		copies[i] 		= this.get(i);
+		placeholders[i] = document.createComment('');
+		copies[i].parentNode.replaceChild(placeholders[i],copies[i]);
+	}
+
+	for (i=0; i<copies.length; i++) {
+		placeholders[i].parentNode.replaceChild(copies[permutation[i]],placeholders[i]);
+	}
+
+	return this;
+
+};
