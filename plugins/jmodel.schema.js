16636792a5b5cdc5bb89f5818503a6439bf33f5f
sharpblue100 <sharpblue100@cd21a03a-4e8f-11de-84bb-6bba061bd1d3>
2010-03-30 14:52:23 +0000

    Initial implementation of creating domain model from OData service metadata.

--- plugins/jmodel.schema.js
+++ plugins/jmodel.schema.js
@@ -6,49 +6,199 @@
  *	Dual licensed under the MIT and GPL licenses
  *
  */
- 
-jModel.plugin.entitytype.pushOneToManyRelationship = function (accessor) {
 
-    var relationships = this.constructor.prototype.hasMany,
-        context = this.context,
-        entitytype = this;
+ (function () {
+
+ 	var _ 		= jModel,
+ 		plugin	= _.plugin;
+
+ 	var schemaContext = _.contexts.create('schema');
+ 	
+ 	
+ 	schemaContext.register('Schema', _.Base.extend({
+
+        has:    [	
+                    {	accessor: 'Namespace',		defaultValue: ''	}
+                ],
+       	
+       	hasMany: [  
+       	            {	accessor:       'EntityType',
+       			        prototype:      'EntityType',
+       			        relativeKey:    'Name'  	}
+       			]
+
+    }), {
+    	primaryKey: 'Namespace',
+    	generateKeys: false,
+    	base: true
+    });
     
-    var foundIndex = relationshipIndex(accessor,relationships);
-    if ( foundIndex ) {
-        var specification = relationships[foundIndex];
-        return {
-            to: function (entityTypeName,relationshipName) {
-                var targetEntity = context.entity(entityTypeName);
-          //      removeRelationship(specification.accessor,targetEntity.options.parent);
-                if ( relationshipName ) {
-                    specification.accessor = relationshipName;
-                }
-                targetEntity.constructor.prototype.hasMany.push(specification);
-                return entitytype;
-            }
-        };
-    } else {
-        return false;
-    }
+    schemaContext.register('EntityType', _.Base.extend({
+
+        has:    [  
+                    {	accessor: 'Name',		defaultValue: ''	},
+                    {   accessor: 'PrimaryKey', defaultValue: ''    }
+                ],
+                
+        hasMany:    [
+                        {   accessor:       'Property',
+                            plural:         'Properties',
+                            prototype:      'Property',
+                            relativeKey:    'Name'      },
+                            
+                        {   accessor:       'Relationship',
+                            prototype:      'Relationship',
+                            relativeKey:    'Name'      }
+                    ]
+
+    }), {
+        nameField: 'Name',
+        relativeKey: 'Name',
+    	generateKeys: false,
+    	base: true
+    });
     
-    function relationshipIndex (accessor,relationships) {
-        for ( index in relationships ) {
-            if ( relationships[index].accessor === accessor ) {
-                foundIndex = index;
-                break;
-            }
-        }
-        return foundIndex;
-    }
+    schemaContext.register('Property', _.Base.extend({
+
+        has:    [  
+                    {	accessor: 'Name',		defaultValue: ''	        },
+                    {   accessor: 'Type',       defaultValue: 'Edm.Int32'   }
+                ]
+
+    }), {
+        plural: 'Properties',
+        nameField: 'Name',
+        relativeKey: 'Name',
+    	generateKeys: false,
+    	base: true
+    });
+ 	
+ 	schemaContext.register('Relationship', _.Base.extend({
+
+        has:    [  
+                    {	accessor: 'Name',		defaultValue: ''	        },
+                    {   accessor: 'Type',       defaultValue: 'toMany'      },
+                    {   accessor: 'ToEntity',   defaultValue: ''            },
+                    {   accessor: 'Field',      defaultValue: ''            }
+                ]
+
+    }), {
+        nameField: 'Name',
+        relativeKey: 'Name',
+    	generateKeys: false,
+    	base: true
+    });
     
-    function removeRelationship (accessor,entityName) {
-        while ( entityName ) {
-            var entity = context.entity(entityName),
-                relationships = entity.constructor.prototype.hasMany;
-            relationships.splice(relationshipIndex(accessor,relationships),1);
-            entityName = entity.options.parent;
-        }
+    
+    plugin.context.withSchema = function (namespace) {
         
+        var applicationContext  = this,
+            schema = schemaContext.createSchema({
+                Namespace: namespace
+            });
+        
+        schema.EntityTypes().event('add').subscribe(function (event) {    
+    	    var entitytype = event.object;
+    	    applicationContext.register(
+    	        entitytype.Name(),
+    	        _.Base.extend({ has:[], hasOne:[], hasMany:[] }),
+    	        { primaryKey: entitytype.PrimaryKey() }
+    	    );
+    	});
+
+    	applicationContext.entities.event('add').subscribe(function (event) {
+
+    	    var entitytype = event.object;
+
+    	    schema.EntityTypes(entitytype.name).Properties().event('add').subscribe(function (event) {
+    	        var property = event.object;
+    	        entitytype.addProperty({
+    	            accessor: property.Name()
+    	        });
+    	    });
+
+    	    schema.EntityTypes(entitytype.name).Relationships().event('add').subscribe(function (event) { 
+    	        
+    	        var relationship = event.object,
+    	            specification = {
+    	                accessor: relationship.Name(),
+                		prototype: relationship.ToEntity(),
+                		field: relationship.Field()
+    	            };
+    	        
+    	        if ( relationship.Type() === 'toOne' ) {
+    	            entitytype.addOneToOneRelationship(specification);
+    	        }
+    	        else {
+    	            entitytype.addOneToManyRelationship(specification);
+    	        }
+    	        
+    	    });
+
+    	})
+    	
+    	return this;
+        
+    };
+    
+    
+    plugin.entitytype.addProperty = function (specification) {
+        this.constructor.prototype.has.push(specification);
     }
-     
-};
+    
+    plugin.entitytype.addOneToOneRelationship = function (specification) {
+        this.constructor.prototype.hasOne.push(specification);
+    }
+    
+    plugin.entitytype.addOneToManyRelationship = function (specification) {
+        this.constructor.prototype.hasMany.push(specification);
+    }
+  	
+ 	
+     plugin.entitytype.pushOneToManyRelationship = function (accessor) {
+
+        var relationships = this.constructor.prototype.hasMany,
+            context = this.context,
+            entitytype = this;
+
+        var foundIndex = relationshipIndex(accessor,relationships);
+        if ( foundIndex ) {
+            var specification = relationships[foundIndex];
+            return {
+                to: function (entityTypeName,relationshipName) {
+                    var targetEntity = context.entity(entityTypeName);
+              //      removeRelationship(specification.accessor,targetEntity.options.parent);
+                    if ( relationshipName ) {
+                        specification.accessor = relationshipName;
+                    }
+                    targetEntity.constructor.prototype.hasMany.push(specification);
+                    return entitytype;
+                }
+            };
+        } else {
+            return false;
+        }
+
+        function relationshipIndex (accessor,relationships) {
+            for ( index in relationships ) {
+                if ( relationships[index].accessor === accessor ) {
+                    foundIndex = index;
+                    break;
+                }
+            }
+            return foundIndex;
+        }
+
+        function removeRelationship (accessor,entityName) {
+            while ( entityName ) {
+                var entity = context.entity(entityName),
+                    relationships = entity.constructor.prototype.hasMany;
+                relationships.splice(relationshipIndex(accessor,relationships),1);
+                entityName = entity.options.parent;
+            }
+
+        }
+
+    };
+ 		
+})();
