d4998c62a975f214b14d6d1f1207cf88d0f921ba
sharpblue100 <sharpblue100@cd21a03a-4e8f-11de-84bb-6bba061bd1d3>
2011-01-11 14:07:13 +0000

    Initial addition of jmodel2.js

---
+++ trunk/jmodel2.js
@@ -0,0 +1,114 @@
+/*
+ *	jModel Javascript Library v2.0.0
+ *	http://code.google.com/p/jmodel/
+ *
+ *	Copyright (c) 2009-2010 Richard Baker
+ *	Dual licensed under the MIT and GPL licenses
+ *
+ *	Requires opal.js and emerald.js
+ *
+ */
+
+var jModel = (function () {
+	
+	
+	var external = _;
+	
+	
+	//
+	// Import Emerald
+	//
+	
+	for ( var i in emerald ) {
+		eval('var '+i+' = emerald.'+i);
+		external[i] = emerald[i];
+	}
+	
+	
+	//
+	// EntityType
+	//
+	
+	function EntityType (fields,options) {
+		
+		var entityType = function (data) {
+			ObservableObject.call(this,fields,options);
+			this.set(data);
+			entityType.objects.add(this);
+		};
+		
+		entityType.constructor	= entityType;
+		entityType.prototype	= new ObservableObject();
+		entityType.objects		= new EntitySet(entityType);
+		entityType.find			= delegateTo(entityType.objects,'filter');
+		
+		return entityType;
+		
+	}
+	
+	external.EntityType = EntityType;
+	
+	
+	//
+	// EntitySet
+	//
+	
+	function EntitySet (constructor) {
+		TypedSet.call(this,constructor);
+	}
+	
+	EntitySet.prototype = extend({
+		
+		constructor: EntitySet,
+		
+		create: function (data) {
+			return this.__construct(data);
+		}
+		
+	}, new TypedSet() );
+	
+	
+	//
+	// Primary Key
+	//
+	
+	function key (options) {
+		options = ( typeof options === 'object' ) ? options : {defaultValue:options};
+		return function (object,field,entity_set) {
+			var field		= new IntegerField(object,field,options),
+				constraint 	= new PrimaryKeyConstraint(field,entity_set);
+			return field;
+		};
+	}
+	
+	external.key = key;
+	
+	
+	//
+	// PrimaryKeyConstraint
+	//
+	
+	function PrimaryKeyConstraint (field,entity_set) {
+		
+		if ( field.get() === null || typeof field.get() === 'undefined' ) {
+			console.log('Should generate key');
+		}
+		
+		field.event.subscribe(function (value) {
+			if ( value === null || typeof value === 'undefined' ) {
+				console.log('Should generate key');
+			}
+		});
+		
+	}
+	
+	
+	return external;
+	
+	
+})();
+
+
+if (_ && _.opal_version) {
+	var _ = jModel;
+}
