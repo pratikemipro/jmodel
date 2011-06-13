23cd4717e29266873f4e121a1ac72160373c9685
sharpblue100 <sharpblue100@cd21a03a-4e8f-11de-84bb-6bba061bd1d3>
2011-06-13 20:20:35 +0000

    Reverted last change.

--- library/opal.js
+++ library/opal.js
@@ -283,12 +283,12 @@ define(function (a,b,c,undefined) {
 		// Docs: none
 		memo: function () {
 			var cache = {},
-				fn = this.post(function (r,x) {
-					cache[x] = r;
+				fn = this.post(function () {
+					cache[_slice.call(arguments,1)] = arguments[0];
 				});
-			return function (x) {
-				var value = cache[x];
-				return value !== undefined ? value : fn.call(this,x);
+			return function () {
+				var value = cache[_slice.call(arguments)];
+				return value !== undefined ? value : fn.apply(this,arguments);
 			};
 		},
 		
