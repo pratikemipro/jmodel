1c782104cd622081b3f879214cd54a26f9c0cc63
sharpblue100 <sharpblue100@cd21a03a-4e8f-11de-84bb-6bba061bd1d3>
2011-07-27 11:01:06 +0000

    Added a skeletal JSON plugin for Topaz.

---
+++ trunk/plugins/topaz.json.js
@@ -0,0 +1,28 @@
+/*
+ *	Topaz JSON Plugin v0.1.0
+ *	http://code.google.com/p/jmodel/
+ *
+ *	Copyright (c) 2011 Richard Baker
+ *	Dual licensed under the MIT and GPL licenses
+ *
+ */
+
+define(['jmodel/topaz'], function (topaz) {
+	
+	'use strict';
+	
+	Object.extend(topaz.ObservableObject.prototype, {
+		
+		toBareObject: function () {
+			return topaz.Set.fromArray(this.fields).reduce(function (object,field) {
+				return object.setProperty(field.field,field.value.toJSON ? field.value.toJSON() : field.value);
+			}, new topaz.EnhancedObject() );
+		},
+		
+		toJSON: function () {
+			return JSON.stringify(this.toBareObject());
+		}
+		
+	});
+	
+});
