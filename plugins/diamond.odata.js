00a8af822b9601925a29ab42afef98fc122e070a
sharpblue100 <sharpblue100@cd21a03a-4e8f-11de-84bb-6bba061bd1d3>
2011-08-03 12:58:09 +0000

    Added functionality for converting OData to objects, and added a basic persistence mechanism (that should only be used for updates at the moment as it doesn’t update entities with primary key values from the database).

--- plugins/diamond.odata.js
+++ plugins/diamond.odata.js
@@ -7,9 +7,82 @@
  *
  */
 
-define(['jmodel/diamond'], function (diamond) {
+define([
+
+	'jmodel/diamond',
+	'jquery',
+	'jmodel-plugins/topaz.json'
+
+], function (diamond,$) {
 	
 	'use strict';
+	
+	Array.fromOData = function (odata,expand) {
+		expand = typeof expand !== 'undefined' ? expand : false;
+		var entity = odata.d ? odata.d : odata,
+			obj = [];
+		for ( var i in entity ) {
+			obj.push(Object.fromOData(entity[i],expand));
+		}
+		return obj;
+	};
+	
+	Date.fromOData = function (odata) {
+		return Date.create(parseInt(odata.substring(6),10));
+	};
+	
+	Object.fromODataUri = function (uri) {
+		var obj;
+		$.ajax({
+			url: uri,
+			type: 'GET',
+			contentType: 'application/json',
+			dataType: 'json',
+			async: false,
+			success: function (data) {
+				console.log(arguments);
+				obj = Object.fromOData(data,false);
+			}
+		});
+		return obj;
+	};
+	
+	Object.fromOData = function (odata,expand) {
+	
+		expand = typeof expand !== 'undefined' ? expand : false;
+	
+		var entity = odata.d ? odata.d : odata,
+			obj;
+
+		if ( entity instanceof Array ) {
+			return Array.fromOData(odata,expand);
+		}
+		else {
+
+			obj = {};
+
+			// Process fields
+			for ( var field in entity ) {
+
+				var value = entity[field];
+				
+				// Unless the field is metadata or a deferred value that should not be epanded
+				if ( field !== '__metadata' && field !== '__proto__' && ( !(value && value.__deferred) || expand ) ) {
+					
+					obj[field] =   typeof value === 'string' && value.substring(1,5) === 'Date' ? Date.fromOData(value)
+								 : value && value.__deferred ? Object.fromODataUri(value.__deferred.uri)
+								 : typeof value === 'object' && value !== null ?  Object.fromOData(value,expand)
+								 : value;
+					
+				}
+				
+			}
+
+		}
+
+		return obj;
+		
+	};
 	
 	Object.extend(diamond.Context.prototype,{
 		serviceLocation: ''
@@ -18,7 +91,23 @@ define(['jmodel/diamond'], function (diamond) {
 	Object.extend(diamond.Entity.prototype,{
 		
 		persist: function () {
-			console.log('Persisting...');
+			
+			var id = this.primaryKeyField ? this[this.primaryKeyField]() : undefined;
+						
+			diamond.event.fromAjax({
+				url: this.context.serviceLocation+'/'+(this.entityType.plural ? this.entityType.plural : this.entityType.typeName+'s')+(id > 0 ? '('+id+')' : ''),
+				type: id > 0 ? 'MERGE' : 'POST',
+				data: JSON.stringify(this.toBareObject().removeProperties(this.key)),
+				contentType: 'application/json',
+				dataType: 'json'
+			})
+			.where(diamond.not(diamond.eq(undefined)))
+			.subscribe(function (json) {
+				console.log(Object.fromOData(json));
+			});
+			
+			return this;
+			
 		}
 		
 	});
