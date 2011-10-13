f7c99a2b0df3b9b0c09cf512d547f1651b5bdee1
sharpblue100 <sharpblue100@cd21a03a-4e8f-11de-84bb-6bba061bd1d3>
2011-10-13 15:23:30 +0000

    Completed tests for EntityTypes.

--- trunk/tests/test-diamond.js
+++ trunk/tests/test-diamond.js
@@ -29,7 +29,13 @@ define(['../library/diamond.js'], function (diamond) {
 			
 		equals( entityTypes.context instanceof diamond.Context, true, 'Constructor sets context');
 		equals( entityTypes instanceof diamond.TypedMap, true, 'EntityTypes is a TypedMap');
-		equals( entityTypes.ensure({forename:String},{}) instanceof Function, true, 'ensure method returns a constructor' )
+		equals( entityTypes.ensure({forename:String},{}) instanceof Function, true, 'ensure method returns a constructor' );
+		
+		var person = entityTypes.create('Person',{forename:String},{});
+		
+		equals( entityTypes.get('Person'), person, 'create method creates and returns an entity type');
+		equals( person instanceof Function, true, 'Created entity type is a constructor');
+		equals( person.options.name, 'Person', 'create method sets name from key correctly in options');
 		
 	});
 
