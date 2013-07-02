9e8556e68063b45d2c26da331ec3223ed48f8236
sharpblue100 <sharpblue100@cd21a03a-4e8f-11de-84bb-6bba061bd1d3>
2013-07-02 09:41:08 +0000

    Added tests of composite fields in Record.

--- trunk/src/library/sapphire/Record.test.coffee
+++ trunk/src/library/sapphire/Record.test.coffee
@@ -1,14 +1,16 @@
 	module 'Record'
 	
 	test 'Constructor', ->
-		
+
 		Person = Record.Of
 			name: String
 			dob: Date
+			affiliations: Set.Of(String)
 			
 		einstein = new Person
 			name: 'Albert Einstein'
 			dob: '1879-3-14'
+			affiliations: ['Swiss Patent Office','Insititute for Advanced Study']
 			
 		console.log einstein
 		
@@ -17,6 +19,9 @@
 		
 		equals einstein.dob() instanceof Date, true, 'Creates date fields of correct type'
 		equals einstein.dob().toDateString(), 'Fri Mar 14 1879', 'Creates string fields with correct value'
+		
+		equals einstein.affiliations() instanceof Set, true, 'Creates composite fields of correct type'
+		deepEqual ( org for org in einstein.affiliations() ), ['Swiss Patent Office','Insititute for Advanced Study'], 'Passes correct constructor arguments'
 		
 		einstein.name 'Al Einstein'
 		
--- trunk/tests/test-library.js
+++ trunk/tests/test-library.js
@@ -1896,20 +1896,33 @@ define(['jmodel/topaz2'], function() {
   });
   module('Record');
   test('Constructor', function() {
-    var Person, einstein;
+    var Person, einstein, org;
     Person = Record.Of({
       name: String,
-      dob: Date
+      dob: Date,
+      affiliations: Set.Of(String)
     });
     einstein = new Person({
       name: 'Albert Einstein',
-      dob: '1879-3-14'
+      dob: '1879-3-14',
+      affiliations: ['Swiss Patent Office', 'Insititute for Advanced Study']
     });
     console.log(einstein);
     equals(typeof einstein.name() === 'string', true, 'Creates string fields of correct type');
     equals(einstein.name(), 'Albert Einstein', 'Creates string fields with correct value');
     equals(einstein.dob() instanceof Date, true, 'Creates date fields of correct type');
     equals(einstein.dob().toDateString(), 'Fri Mar 14 1879', 'Creates string fields with correct value');
+    equals(einstein.affiliations() instanceof Set, true, 'Creates composite fields of correct type');
+    deepEqual((function() {
+      var _i, _len, _ref, _results;
+      _ref = einstein.affiliations();
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        org = _ref[_i];
+        _results.push(org);
+      }
+      return _results;
+    })(), ['Swiss Patent Office', 'Insititute for Advanced Study'], 'Passes correct constructor arguments');
     einstein.name('Al Einstein');
     equals(einstein.name(), 'Al Einstein', true, 'String fields can be updated');
     einstein.name(function(name) {
